6e97b5d67f38f0970f1f8f06d9cf25112820707d
Rami Reini <ramireini@gmail.com>
2019-05-23 15:17:54 +0300

    .

--- code/build.sh
+++ code/build.sh
@@ -2,6 +2,6 @@ clear
 mkdir -p ../build
 cp -RTv ../data ../build/data/
 
-gcc linux_moonbreath.c -Wall -Wextra -O2 -std=c99 -DMOONBREATH_DEBUG=1 -o ../build/moonbreathgayboi -lSDL2 -lSDL2_image -lSDL2_ttf -lm
+gcc linux_moonbreath.c -Wall -Wextra -O2 -std=c99 -DMOONBREATH_DEBUG=1 -o ../build/moonbreath -lSDL2 -lSDL2_image -lSDL2_ttf -lm
 
 echo [COMPLETE]
